0226d22d710475d1e4a74dc5ce3fc6bf66300947
yurystar <staradubtsau_yury@mail.ru>
2022-01-25 17:00:42 +0300

    First commit

--- com.senla.elhoteladmin.consoleUI/src/consoleuserinterface/utils/CSVRead.java
+++ com.senla.elhoteladmin.consoleUI/src/consoleuserinterface/utils/CSVRead.java
@@ -1,9 +1,9 @@
 package consoleuserinterface.utils;
 
+import com.senla.elhoteladmin.dao.AdditionalServiceDaoImpl;
+import com.senla.elhoteladmin.dao.GuestDaoImpl;
 import com.senla.elhoteladmin.dao.RoomDaoImpl;
-import com.senla.elhoteladmin.entity.Room;
-import com.senla.elhoteladmin.entity.RoomStatus;
-import com.senla.elhoteladmin.entity.RoomType;
+import com.senla.elhoteladmin.entity.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -18,7 +18,7 @@ public class CSVRead {
 
             String[] lineArray;
             String line;
-            List<Room> roomArrayList = new ArrayList<>();
+            List<Room> roomsFromFile = new ArrayList<>();
             while ((line = reader.readLine()) != null) {
 
                 lineArray = line.split(",");
@@ -29,22 +29,87 @@ public class CSVRead {
                 room.setRoomPlaces(Integer.parseInt(lineArray[3]));
                 room.setRoomPrice(Integer.parseInt(lineArray[4]));
                 room.setRoomStatus(RoomStatus.valueOf(lineArray[5]));
-                roomArrayList.add(room);
+                roomsFromFile.add(room);
             }
 
-            System.out.println(roomArrayList);
+            System.out.println(roomsFromFile);
             System.out.println();
-            for (Room room : roomArrayList) {
-                Room roomPresent = roomDao.getRoomByNum(room.getRoomNumber());
-                if (roomPresent == null) {
-                    roomDao.save(room);
-                } else if (!room.equals(roomPresent)) {
-                    roomPresent.setRoomID(room.getRoomID());
-                    roomPresent.setRoomType(room.getRoomType());
-                    roomPresent.setRoomPlaces(room.getRoomPlaces());
-                    roomPresent.setRoomPrice(room.getRoomPrice());
-                    roomPresent.setRoomStatus(room.getRoomStatus());
-                    roomDao.update(room);
+
+            for (Room roomFromImport : roomsFromFile) {
+                Room roomFromStorage = roomDao.getRoomByNum(roomFromImport.getRoomNumber());
+                if (roomFromStorage == null) {
+                    roomDao.save(roomFromImport);
+                } else if (!roomFromImport.equals(roomFromStorage)) {
+                    roomDao.update(roomFromImport);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error read!");
+        }
+    }
+
+    public static void CSVReadGuest() {
+        GuestDaoImpl guestDao = GuestDaoImpl.getInstance();
+        try (BufferedReader reader = new BufferedReader(new FileReader("..\\Guest.csv"))) {
+
+            String[] lineArray;
+            String line;
+            List<Guest> guestsFromFile = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+
+                lineArray = line.split(",");
+                Guest guest = new Guest();
+                guest.setGuestID(Integer.parseInt(lineArray[0]));
+                guest.setGuestPassport(Integer.parseInt(lineArray[1]));
+                guest.setGuestName(lineArray[2]);
+                guest.setGuestSurname(lineArray[3]);
+                guestsFromFile.add(guest);
+            }
+
+            System.out.println(guestsFromFile);
+            System.out.println();
+
+            for (Guest guestFromImport : guestsFromFile) {
+                Guest guestFromStorage = guestDao.get(guestFromImport.getGuestID());
+                if (guestFromStorage == null) {
+                    guestDao.save(guestFromImport);
+                } else if (!guestFromImport.equals(guestFromStorage)) {
+                    guestDao.update(guestFromImport);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error read!");
+        }
+    }
+
+    public static void CSVReadAddServ() {
+        AdditionalServiceDaoImpl serviceDao = AdditionalServiceDaoImpl.getInstance();
+        try (BufferedReader reader = new BufferedReader(new FileReader("..\\AdditionalService.csv"))) {
+
+            String[] lineArray;
+            String line;
+            List<AdditionalService> addServsFromFile = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+
+                lineArray = line.split(",");
+                AdditionalService service = new AdditionalService();
+                service.setServiceID(Integer.parseInt(lineArray[0]));
+                service.setServiceName(lineArray[1]);
+                service.setServicePrice(Integer.parseInt(lineArray[2]));
+                addServsFromFile.add(service);
+            }
+
+            System.out.println(addServsFromFile);
+            System.out.println();
+
+            for (AdditionalService addServFromImport : addServsFromFile) {
+                AdditionalService addServFromStorage = serviceDao.get(addServFromImport.getServiceID());
+                if (addServFromStorage == null) {
+                    serviceDao.save(addServFromImport);
+                } else if (!addServFromImport.equals(addServFromStorage)) {
+                    serviceDao.update(addServFromImport);
                 }
             }
         } catch (Exception e) {
--- com.senla.elhoteladmin.consoleUI/src/consoleuserinterface/utils/CSVWrite.java
+++ com.senla.elhoteladmin.consoleUI/src/consoleuserinterface/utils/CSVWrite.java
@@ -1,6 +1,10 @@
 package consoleuserinterface.utils;
 
+import com.senla.elhoteladmin.dao.AdditionalServiceDaoImpl;
+import com.senla.elhoteladmin.dao.GuestDaoImpl;
 import com.senla.elhoteladmin.dao.RoomDaoImpl;
+import com.senla.elhoteladmin.entity.AdditionalService;
+import com.senla.elhoteladmin.entity.Guest;
 import com.senla.elhoteladmin.entity.Room;
 
 import java.io.*;
@@ -18,17 +22,78 @@ public class CSVWrite {
             while (index != 0) {
                 System.out.println("Введите номер комнаты для экспорта. Или введите 0 для завершения выбора.");
                 index = ScanUtil.getInt();
-                if (roomDao.getRoomByNum(index) != null) {
+                if (roomDao.getRoomByNum(index) != null && index != 0) {
                     roomsForExport.add(roomDao.getRoomByNum(index));
-                } else System.out.println("Нет такой комнаты или выбор завершен.");
+                } else System.out.println("Нет такой комнаты.");
             }
-            System.out.println("Комнаты " + roomsForExport + "\n записаны в файл.");
-            System.out.println();
+            System.out.println("Выбор завершен.\n");
+
             for (Room room : roomsForExport) {
                 String roomStr = room.getRoomID() + "," + room.getRoomNumber() + "," + room.getRoomType() + "," +
                         room.getRoomPlaces() + "," + room.getRoomPrice() + "," + room.getRoomStatus() + "\n";
                 writer.write(roomStr);
             }
+
+            System.out.println("Комнаты " + roomsForExport + "\n записаны в файл.\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error. Не удалось записать в файл.");
+        }
+    }
+
+    public static void CSVWriteGuest() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("..\\Guest.csv"))) {
+            GuestDaoImpl guestDao = GuestDaoImpl.getInstance();
+            List<Guest> guestsForExport = new ArrayList<>();
+
+            int index = -1;
+            while (index != 0) {
+                System.out.println("Введите ID номер гостя для экспорта. Или введите 0 для завершения выбора.");
+                index = ScanUtil.getInt();
+                if (guestDao.get(index) != null && index != 0) {
+                    guestsForExport.add(guestDao.get(index));
+                } else System.out.println("Нет такого гостя.");
+            }
+            System.out.println("Выбор завершен.\n");
+
+            for (Guest guest : guestsForExport) {
+                String guestStr = guest.getGuestID() + "," + guest.getGuestPassport() + "," +
+                        guest.getGuestName() + "," + guest.getGuestSurname() + "\n";
+                writer.write(guestStr);
+            }
+
+            System.out.println("Комнаты " + guestsForExport + "\n записаны в файл.\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error. Не удалось записать в файл.");
+        }
+    }
+
+    public static void CSVWriteAddServ() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("..\\AdditionalService.csv"))) {
+            AdditionalServiceDaoImpl serviceDao = AdditionalServiceDaoImpl.getInstance();
+            List<AdditionalService> servicesForExport = new ArrayList<>();
+
+            int index = -1;
+            while (index != 0) {
+                System.out.println("Введите ID номер дополнительного сервиса для экспорта. " +
+                        "Или введите 0 для завершения выбора.");
+                index = ScanUtil.getInt();
+                if (serviceDao.get(index) != null && index != 0) {
+                    servicesForExport.add(serviceDao.get(index));
+                } else System.out.println("Нет такого дополнительного сервиса.");
+            }
+            System.out.println("Выбор завершен.\n");
+
+            for (AdditionalService service : servicesForExport) {
+                String serviceStr = service.getServiceID() + "," + service.getServiceName() +
+                        service.getServicePrice() + "\n";
+                writer.write(serviceStr);
+            }
+
+            System.out.println("Дополнительные сервисы " + servicesForExport + "\n записаны в файл.\n");
 
         } catch (Exception e) {
             e.printStackTrace();
